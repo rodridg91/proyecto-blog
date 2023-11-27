bfcd4d4ee9109bf8d4bc2633491b1ef5b47a4360
Rodrigo <rodrigo.g.dominguez.g@gmail.com>
2023-11-27 20:11:20 -0300

    Creada la app Noticias

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from blog import views
+from . import views
 
+app_name = 'noticias'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.Home, name='home'),
-    path('nosotros/', views.Nosotros, name='nosotros'),
-    path('noticias/', include('apps.noticias.urls')),
+    path('', views.ListarNoticias, name='listar'),
 ]
---
+++ apps/noticias/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def ListarNoticias(request):
+    return render(request, 'noticias/listar.html')
